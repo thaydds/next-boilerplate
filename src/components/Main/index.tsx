aad24866ef1ef7a6d0b66046e001ddb5cef1ffaa
thaydds <thayronedds@gmail.com>
2021-02-11 07:35:29 -0300

    ffirst component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>react avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="atom image with react avançado text" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" />
+  </S.Wrapper>
 )
 
 export default Main
